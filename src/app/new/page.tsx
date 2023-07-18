833f3868464242217e7927184614a75d8b5f6c7e
Aandi1989 <navaliny1989@gmail.com>
2023-07-18 10:40:07 +0300

    completed

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,5 @@
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
 async function createTodo(data: FormData) {
@@ -6,6 +8,8 @@ async function createTodo(data: FormData) {
     if(typeof title !== "string" || title.length ===0){
         throw new Error ("Invalid Title")
     }
+    await prisma.todo.create({data :{title, complete: false}})
+    redirect("/")
 }
 
 export default function Page() {
